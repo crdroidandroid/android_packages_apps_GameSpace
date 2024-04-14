eb3f0d2268c7174b80c00e0a018e07f50bef5d26
Pranav Vashi <neobuddy89@gmail.com>
2024-04-14 09:31:45 +0530

    Revert "Revert "Revert "Remove unimplemented notification function"""
    This reverts commit c53e94de1acc245c3d04a45d7f7b05f4c12724c9.

--- app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/DanmakuServiceListener.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/DanmakuServiceListener.kt
@@ -32,7 +32,7 @@ class DanmakuServiceListener : NotificationListenerService() {
     var danmakuServiceInterface: DanmakuServiceInterface? = null
 
     override fun onNotificationPosted(sbn: StatusBarNotification) {
-    if (!(danmakuServiceInterface?.danmakuNotificationMode ?: false) || !sbn.isClearable || sbn.isOngoing) return
+    if (!(danmakuServiceInterface?.danmakuNotificationMode ?: false) || !sbn.isClearable || sbn.isOngoing || sbn.getIsContentSecure()) return
 
         val extras = sbn.notification.extras
         val title = extras.getString(Notification.EXTRA_TITLE) ?: extras.getString(Notification.EXTRA_TITLE_BIG)
