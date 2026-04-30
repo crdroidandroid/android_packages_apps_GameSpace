4b25cabcde997a8367d43133d28637601cc054a0
Pranav Vashi <neobuddy89@gmail.com>
2026-04-30 21:53:02 +0530

    GameSpace: Do not set process priority from sidebar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameSideBar.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameSideBar.kt
@@ -26,7 +26,6 @@ import android.graphics.PixelFormat
 import android.graphics.Rect
 import android.os.Bundle
 import android.os.Handler
-import android.os.Process
 import android.os.UserHandle
 import android.provider.Settings
 import android.view.*
@@ -283,8 +282,6 @@ class GameSidebar(
         panelView = pv
 
         try {
-            Process.setThreadGroupAndCpuset(Process.myPid(), Process.THREAD_GROUP_TOP_APP)
-            Process.setProcessGroup(Process.myPid(), Process.THREAD_GROUP_TOP_APP)
             wm.addView(pv, panelLayoutParam)
             gameBarView.visibility = View.GONE
         } catch (_: Exception) {
@@ -308,10 +305,6 @@ class GameSidebar(
         panelView?.let { pv ->
             runCatching { wm.removeViewImmediate(pv) }
             panelView = null
-        }
-        runCatching {
-            Process.setThreadGroupAndCpuset(Process.myPid(), 9)
-            Process.setProcessGroup(Process.myPid(), 9)
         }
 
         if (!shouldClose) {
